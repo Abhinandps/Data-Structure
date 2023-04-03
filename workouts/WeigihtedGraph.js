c2cf6b06950062ee65b004d92dbbd21780e4acb0
Abhinand <psabhinand333s@gmail.com>
2023-04-03 20:52:55 +0530

    Weighted Graph Implementation

---
+++ workouts/WeigihtedGraph.js
@@ -0,0 +1,55 @@
+
+class WeightedGraph{
+    constructor(){
+        this.adjacentList = {}
+    }
+
+    addVertex(vertex){
+        if(!this.adjacentList[vertex]){
+            this.adjacentList[vertex] = new Set()
+        }
+    }
+
+    addEdges(vertex1,vertex2,weight){
+        if(!this.adjacentList[vertex1]){
+            this.addVertex(vertex1)
+        }
+        if(!this.adjacentList[vertex2]){
+            this.addVertex(vertex2)
+        }
+
+        this.adjacentList[vertex1].add({node:vertex2,weight:weight})
+        this.adjacentList[vertex2].add({node:vertex1,weight:weight})
+    }
+
+    removeEdges(vertex1,vertex2){
+        
+    }
+
+    display(){
+        let list =''
+        for(let vertex in this.adjacentList){
+            list+=`${vertex} -> `
+            for(let value of this.adjacentList[vertex]){
+                list+=`${value.node} ${value.weight} `
+            }
+            list+=`\n`
+        }
+        console.log(list);
+    }
+}
+
+const g = new WeightedGraph()
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+
+g.addEdges("A","B",5)
+g.addEdges("B","C",2)
+
+g.display()
+
+// console.log(g.adjacentList);
+
+
+
