0664c865bcb5f0aabc9435af5ee6d3cdbc2696c6
AbhinandPS <psabhinand333s@gmail.com>
2024-01-14 00:11:16 +0530

    Value equal to index value

--- QustionsWorkout/Geeksforgeeks/school/1.js
+++ QustionsWorkout/Geeksforgeeks/school/1.js
@@ -1,5 +1,5 @@
 // Sum of Series
-
+// =================
 /*
 
 Write a program to find the sum of the given series 1+2+3+ . . . . . .(N terms) 
@@ -28,4 +28,43 @@ class Solution {
 } 
 
 // Expected Time Complexity: O(1)
+// Expected Auxiliary Space: O(1)
+
+// ------------------------------------------------------------------
+
+// Value equal to index value
+// ===========================
+
+/*
+
+Given an array Arr of N positive integers. Your task is to find the elements whose value is equal to that of its index value ( Consider 1-based indexing ).
+
+Note: There can be more than one element in the array which have the same value as its index. You need to include every such element's index. Follows 1-based indexing of the array.
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {15, 2, 45, 12, 7}
+Output: 2
+Explanation: Only Arr[2] = 2 exists here.
+
+*/
+
+
+class Solution{
+    valueEqualToIndex(arr,n){
+        const result = [];
+        
+        for(let i=1; i<=n;i++){
+            if(arr[i - 1] === i){
+                result.push(i);
+            }
+        }
+        return result;
+    }
+}
+
+
+// Expected Time Complexity: O(N)
 // Expected Auxiliary Space: O(1)
