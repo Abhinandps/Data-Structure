e76012fa782c08bd5ef8a51e0ed332aadf4772be
AbhinandPS <psabhinand333s@gmail.com>
2024-01-19 14:50:42 +0530

    Running Sum of 1d Array

--- week 1/BruteForce.js
+++ week 1/BruteForce.js
@@ -66,3 +66,34 @@ function minValue(nums,start,end){
         return minValue(nums,start, end-1);
     }
 }
+
+// ----------------------------------------------------------
+
+// Running Sum of 1d Array
+// =========================
+
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+// Example 3:
+
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+ 
+
+var runningSum = function(nums) {
+    let currentSum = 0;
+    for(let i=0; i<nums.length; i++){
+        currentSum +=nums[i]
+        nums[i]=currentSum;
+    }
+    return nums
+};
