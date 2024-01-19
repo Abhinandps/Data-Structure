dc63d5a9634a4ccd90293756816fe1dfd1ee7cfb
AbhinandPS <psabhinand333s@gmail.com>
2024-01-19 14:40:50 +0530

    Find Minimum in Rotated Sorted Array II

--- week 1/BruteForce.js
+++ week 1/BruteForce.js
@@ -23,3 +23,46 @@ const newArray = [20, 50, 2, 321, 8, 121, 256, 133, 32]
 const maxValue = bruteForce(newArray);
 
 console.log(`Max value of this array is: ${maxValue}`); // 321
+
+// -----------------------------------------------------------------------
+
+
+// Find Minimum in Rotated Sorted Array II
+
+// Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
+
+// [4,5,6,7,0,1,4] if it was rotated 4 times.
+// [0,1,4,4,5,6,7] if it was rotated 7 times.
+// Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+
+
+// Example 1:
+
+// Input: nums = [1,3,5]
+// Output: 1
+// Example 2:
+
+// Input: nums = [2,2,2,0,1]
+// Output: 0
+ 
+
+
+var findMin = function(nums) {
+    return minValue(nums,0,nums.length-1)
+};
+
+function minValue(nums,start,end){
+    if(start === end){
+        return nums[start];
+    }
+    let mid = Math.floor((start + end) / 2);
+    if(nums[mid] > nums[end]){
+        return minValue(nums, mid+1,end)
+    }else if(nums[mid] < nums[end]){
+        return minValue(nums,start,mid)
+    }
+    else{
+        return minValue(nums,start, end-1);
+    }
+}
