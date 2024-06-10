6485e52e51320618fc3c7f574d09e4767a77196f
Abhinand P S <67689997+Abhinandps@users.noreply.github.com>
2024-06-10 13:13:16 +0530

    Time: 48 ms (83.98%), Space: 48.9 MB (65.1%) - LeetHub

--- 1137-height-checker/1137-height-checker.js
+++ 1137-height-checker/1137-height-checker.js
@@ -3,18 +3,12 @@
  * @return {number}
  */
 var heightChecker = function(heights) {
-    let expected = [];
-
-    // 1,1,4,2,1,3
-    heights.forEach((height, i)=> expected[i] = height);
-    // 1,1,1,2,3,4
-    expected.sort((a,b)=> a - b);
-    let indices = 0;
-
-    expected.forEach((height, i)=>{
-        if(height !== heights[i]){
-            indices++;
+    let ans =0;
+    const sorted = heights.slice().sort((a,b) => a - b);
+    for(let i = 0; i < heights.length ; i++){
+        if(sorted[i] !== heights[i]){
+            ans++;
         }
-    })
-    return indices;
+    }
+    return ans;
 };
